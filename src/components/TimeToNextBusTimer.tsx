704de9fd08bd22755f3b09ef51f6e200a90fdf29
JarekMyjak <jarekmyjak99@gmail.com>
2023-10-07 23:45:47 +0200

    test 4

--- src/components/TimeToNextBusTimer.tsx
+++ src/components/TimeToNextBusTimer.tsx
@@ -11,23 +11,25 @@ type TimeToNextBusTimerProps = {
 export default function TimeToNextBusTimer({ rawStopTimes }: TimeToNextBusTimerProps) {
 	// const timeFormater = new Intl.DateTimeFormat("pl-PL", { hour: "numeric", minute: "2-digit" })
 
-	const now = new Date();
+	const now = new Date().getTime();
 	const offset = getTimezoneOffset('Europe/Warsaw');
 	const utcTimeStamp = new Date(Date.now()+(new Date().getTimezoneOffset()*60000)).getTime()
-	const relativeOffset = offset - (now.getTime() - utcTimeStamp);
+	const relativeOffset = offset - (now - utcTimeStamp);
 
-	console.log(now.getTime(), utcTimeStamp, now.getTime() - utcTimeStamp, offset)
+	// console.log(now.getTime(), utcTimeStamp, now.getTime() - utcTimeStamp, offset)
 
 	const stopTimes = rawStopTimes.map((rawTime) => {
 		const [hours, minutes] = rawTime.split(":").map(n => Number(n));
 
-		const timeString = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate()} ${hours}:${minutes.toString().padStart(2, '0')}:59`
-		const utcDate = zonedTimeToUtc(timeString, 'Europe/Warsaw')
-
+		// const timeString = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate()} ${hours}:${minutes.toString().padStart(2, '0')}:59`
+		
+		const adjustedDate = new Date(now + relativeOffset);
+		adjustedDate.setHours(hours, minutes)
 		// const dateTimeLocale = format(zonedDate, pattern, { timeZone: "Australia/Lord_Howe" })
 
-		// console.log('timenow', now,'timenow warsaw', zonedDate, hours + ":" + minutes, rawTime);
-		return utcDate;
+		console.log(adjustedDate.getHours(), adjustedDate.getMinutes(), hours + ":" + minutes, rawTime);
+		setTest(t => [...t, adjustedDate])
+		return adjustedDate;
 	});
 
 	function findNextTimeIndex() {
@@ -39,21 +41,23 @@ export default function TimeToNextBusTimer({ rawStopTimes }: TimeToNextBusTimerP
 	}
 
 	const [nextStopTimeIndex, setNextStopTimeIndex] = createSignal(findNextTimeIndex());
+	const [test, setTest] = createSignal<Date[]>([]);
 
 	const interval = setInterval(() => {
 		const foundNextTime = findNextTimeIndex()
 		setNextStopTimeIndex(foundNextTime !== -1 ? foundNextTime : 0);
-		console.log(now.getTime(), utcTimeStamp, now.getTime() - utcTimeStamp)
+		// console.log(now.getTime(), utcTimeStamp, now.getTime() - utcTimeStamp)
 
 	}, 1000);
 	onCleanup(() => clearInterval(interval));
 
 
 	return (<>
-		{now.getTime()}{<br/>}
+		{/* {now.getTime()}{<br/>} */}
 		{utcTimeStamp}{<br/>}
-		{now.getTime() - utcTimeStamp}{<br/>}
+		{/* {now.getTime() - utcTimeStamp}{<br/>} */}
 		{relativeOffset}{<br/>}
+		{test().join('\n')}
 		{/* {now.toLocaleString()}<br /> */}
 		{/* {now.toLocaleString("pl-PL")} */}
 		{/* <span class="nextBusTimer">{format(stopTimes[nextStopTimeIndex()], "mm:ss")}</span>
