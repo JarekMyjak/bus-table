273ae6a2cd697eed9235e92e0e3c30fef953226c
Jarosław Myjak <jarekmyjak99@gmail.com>
2024-11-17 15:31:25 +0100

    add basic scraper build functionality

---
+++ scrapeConverted.js
@@ -0,0 +1,75 @@
+import puppeteer from 'puppeteer';
+import { writeFileSync } from 'fs';
+function slugify(str) {
+    str = str.normalize('NFKD');
+    str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
+    str = str.toLowerCase(); // convert string to lowercase
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+        .replace(/\s+/g, '-') // replace spaces with hyphens
+        .replace(/-+/g, '-'); // remove consecutive hyphens
+    return str;
+}
+const browser = await puppeteer.launch({ headless: true });
+const page = await browser.newPage();
+const linesToScrape = [{
+        number: 11,
+        color: "#da9100",
+    }, {
+        number: 14,
+        color: "#76ae40",
+    }, {
+        number: 18,
+        color: "#344496",
+    }, {
+        number: 19,
+        color: "#6cabe1",
+    }];
+const stops = {};
+const lines = [];
+for (const line of linesToScrape) {
+    for (const direction of [0, 1]) {
+        const lineStops = {};
+        await page.goto(`https://www.rozkladzik.pl/zakopane/rozklad_jazdy.html?l=${line.number}&d=${direction}&b=0&dt=0`);
+        // await page.screenshot({ path: `${line}.png` })
+        const stopNames = await page.evaluate(() => {
+            return [...document.querySelectorAll('.bs a')].map((element) => { var _a; return (_a = element.textContent) !== null && _a !== void 0 ? _a : (element.textContent = ''); });
+        });
+        for (const [stopIndex, stopName] of stopNames.entries()) {
+            const stopSlug = slugify(stopName);
+            stops[stopSlug] = { name: stopName };
+            await page.goto(`https://www.rozkladzik.pl/zakopane/rozklad_jazdy.html?l=${line.number}&d=${direction}&b=${stopIndex}&dt=0`);
+            const stopTimes = await page.evaluate(() => {
+                var _a;
+                const timesArr = [];
+                const rowElements = [...document
+                        .querySelectorAll('#time_table tbody tr')];
+                for (const row of rowElements) {
+                    const hour = (_a = row.querySelector("td.h")) === null || _a === void 0 ? void 0 : _a.textContent;
+                    const minutes = [...row.querySelectorAll("td.m")].map((min) => min === null || min === void 0 ? void 0 : min.textContent);
+                    for (const minuteInstance of minutes) {
+                        timesArr.push(`${hour}:${minuteInstance.split(' ')[0]}`);
+                    }
+                }
+                return timesArr;
+            });
+            lineStops[stopSlug] = {
+                name: stopName, times: stopTimes
+            };
+        }
+        lines.push({
+            name: `${line.number}-${direction}`,
+            color: line.color,
+            stops: lineStops
+        });
+    }
+}
+const output = { stops, lines };
+const jsonOutput = JSON.stringify(output);
+try {
+    writeFileSync('./src/data/busStopData.json', jsonOutput, 'utf8');
+    console.log('Data successfully saved to disk');
+}
+catch (error) {
+    console.log('An error has occurred ', error);
+}
+await browser.close();
